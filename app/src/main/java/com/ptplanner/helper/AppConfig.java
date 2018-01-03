871b529bb83f94c7ad7a6cebab1f421f5179ef59
surajcse116 <suraj.shaw@esolzmail.com>
2018-01-03 14:53:13 +0530

    3 jan 2018

--- app/src/main/java/com/ptplanner/helper/AppConfig.java
+++ app/src/main/java/com/ptplanner/helper/AppConfig.java
@@ -21,8 +21,8 @@ import java.util.Locale;
 
 public class AppConfig {
 
-    public static String HOST = "http://esolz.co.in/lab6/ptplanner/";
-//     public static String HOST = "https://pt-planner.com/";
+//    public static String HOST = "http://esolz.co.in/lab6/ptplanner/";
+     public static String HOST = "https://pt-planner.com/";
 
      public static String selected_date_from_calender = "";
 
